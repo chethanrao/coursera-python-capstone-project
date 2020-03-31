6321b8cfa057f2835db65968ef4ead38a2b8a153
Chethan Rao <meetchethan@gmail.com>
2020-03-31 11:31:26 -0400

    capstone page rank project

---
+++ capstone python/pagerank/spider.js
@@ -0,0 +1,16 @@
+spiderJson = {"nodes":[
+{"weight":5,"rank":19.0, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":3,"rank":17.959784941128028, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":5.366855714183904, "id":3, "url":"http://www.dr-chuck.com/office"},
+{"weight":1,"rank":2.987530386171618, "id":4, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":2.987530386171618, "id":5, "url":"http://www.dr-chuck.com/obi-sample"},
+{"weight":1,"rank":0.0, "id":15, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":0,"target":3,"value":3},
+{"source":0,"target":4,"value":3},
+{"source":4,"target":0,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":5,"value":3},
+{"source":5,"target":1,"value":3}]};
